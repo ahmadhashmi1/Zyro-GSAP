1313a715a962f1c2e3a55993acfd875cff94aa7e
ahmadhashmi1 <ahmadhassanhashmi.work@gmail.com>
2026-08-27 22:55:37 +0500

    added 7cans in animation

--- script.js
+++ script.js
@@ -99,12 +99,21 @@ tl.to("#masala-cola", {
     ease: "power1.inOut"
 },"<")
 
+.to("#zeera-zero",{
+    opacity: 0,
+    duration: 0.8,
+    ease: "power1.inOut"
+  },"<")
 .to("#bolt-zero",{
   opacity: 1,
   duration: 0.8,
   ease: "power1.inOut"
 },"<")
-
+.to("#zeera-zero1",{
+    opacity: 0,
+    duration: 0.8,
+    ease: "power1.inOut"
+  },"<")
 .to("#bolt-zero1",{
     opacity: 1,
     duration: 0.8,
@@ -149,11 +158,21 @@ tl.to("#masala-cola", {
     duration: 0.8,
     ease: "power1.inOut",
 },"<")
+.to("#blue-boolt",{
+    opacity: 0,
+    duration: 0.8,
+    ease: "power1.inOut"
+  },"<")
 .to("#berry-zero",{
     opacity: 1,
     duration: 0.8,
     ease: "poer1.inOut",
 },"<")
+.to("#blue-boolt",{
+    opacity: 0,
+    duration: 0.8,
+    ease: "power1.inOut"
+  },"<")
 .to("#berry-zero1",{
     opacity:1,
     duration:0.8,
@@ -191,12 +210,171 @@ tl.to("#masala-cola", {
     "<0.8"
 )
 .to(".first",{
-    backgroundColor:"rgb(243, 243, 215)",
+    backgroundColor:"rgb(102, 193, 126)",
     duration: 0.8,
     ease :"power1.inOut",
 },"<")
 .to("#heading",{
-    color:"rgb(107, 112, 46)",
+    color:"rgb(1, 116, 59)",
     duration: 0.8,
     ease: "power1.inOut",
 },"<")
+.to("#berry-zero",{
+    opacity: 0,
+    duration: 0.8,
+    ease: "power1.inOut"
+  },"<")
+.to("#ginger-zero",{
+    opacity:1,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+.to("#berry-zero1",{
+    opacity: 0,
+    duration: 0.8,
+    ease: "power1.inOut"
+  },"<")
+.to("#ginger-zero1",{
+    opacity:1,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+
+.to("#ginger-ale", {
+    motionPath: {
+        path: [
+            { x: "0%",    y: "0vh" },
+            { x: "-40%",  y: "0.5vh" },
+            { x: "-75%",  y: "2vh" },
+            { x: "-130%", y: "12vh" },
+            { x: "-180%", y: "35vh" },
+            { x: "-220%", y: "80vh" }
+        ],
+        curviness: 1.5
+    },
+    opacity: 0,
+    duration: 1.5,
+    ease: "power1.inOut"
+})
+
+.fromTo("#tonic-water",
+    {
+        x: "140%",
+        opacity: 0
+    },
+    {
+        x: "0%",
+        opacity: 1,
+        duration: 1.5,
+        ease: "power2.out"
+    },
+    "<0.8"
+)
+.to(".first",{
+    backgroundColor:"rgb(253, 229, 181)",
+    duration: 0.8,
+    ease :"power1.inOut",
+},"<")
+.to("#heading",{
+    color:"rgb(253, 183, 27)",
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+.to("#ginger-zero",{
+    opacity:0,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+.to("#tonic-zero",{
+    opacity:1,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+.to("#ginger-zero1",{
+    opacity:0,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+.to("#tonic-zero1",{
+    opacity:1,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+
+.to("#tonic-water", {
+    motionPath: {
+        path: [
+            { x: "0%",    y: "0vh" },
+            { x: "-40%",  y: "0.5vh" },
+            { x: "-75%",  y: "2vh" },
+            { x: "-130%", y: "12vh" },
+            { x: "-180%", y: "35vh" },
+            { x: "-220%", y: "80vh" }
+        ],
+        curviness: 1.5
+    },
+    opacity: 0,
+    duration: 1.5,
+    ease: "power1.inOut"
+})
+
+.fromTo("#vita-cola",
+    {
+        x: "140%",
+        opacity: 0
+    },
+    {
+        x: "0%",
+        opacity: 1,
+        duration: 1.5,
+        ease: "power2.out"
+    },
+    "<0.8"
+)
+.to(".first",{
+    backgroundColor:"rgb(183, 183, 183)",
+    duration: 0.8,
+    ease :"power1.inOut",
+},"<")
+.to("#heading",{
+    color:"rgb(0, 0, 0)",
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+.to("#tonic-zero",{
+    opacity:0,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+.to("#vita-zero",{
+    opacity:1,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+.to("#tonic-zero1",{
+    opacity:0,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+.to("#vita-zero1",{
+    opacity:1,
+    duration: 0.8,
+    ease: "power1.inOut",
+},"<")
+
+.to("#vita-cola", {
+    motionPath: {
+        path: [
+            { x: "0%",    y: "0vh" },
+            { x: "-40%",  y: "0.5vh" },
+            { x: "-75%",  y: "2vh" },
+            { x: "-130%", y: "12vh" },
+            { x: "-180%", y: "35vh" },
+            { x: "-220%", y: "80vh" }
+        ],
+        curviness: 1.5
+    },
+    opacity: 0,
+    duration: 1.5,
+    ease: "power1.inOut"
+})
